a4a47cd0b795d2d109655a52a0469fe7e981351e
Beginner01-ux <manishlohana01@gmail.com>
2026-07-31 17:02:16 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import pandas as pd
 import requests
 
 # ===== CONFIGURATION =====
-SHEET_URL = "https://docs.google.com/spreadsheets/d/1xrfzqniGYI99m9g9Jf97hmDQUa_a54Kexliur2-frDU/edit?usp=drivesdk"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1xrfzqniGYl99m9g9Jf97hmDQUa_a54KexIiur2-frDU/edit"
 SHEET_NAME = "exam_serial_numbers"
 
 # ===== PAGE SETUP =====
 st.set_page_config(page_title="Exam PDF Access", layout="centered")
-st.title(" Exam Paper Access Portal")
+st.title("Student Databse Portal")
+st.title("by: MANISH LOHANA")
 st.write("Enter your serial number to download your exam paper")
 
 # ===== LOAD DATA FROM GOOGLE SHEET =====
